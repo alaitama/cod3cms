f3a869b7713ed87f1b61b62590d606d8bd66bd70
ramsys <ramborghacklab@gmail.com>
2015-07-26 23:55:54 +0200

    modified:   wsgi/openshift/custom_storages.py Added function isdir

--- wsgi/openshift/custom_storages.py
+++ wsgi/openshift/custom_storages.py
@@ -1,9 +1,14 @@
 
 from django.conf import settings
 from storages.backends.s3boto import S3BotoStorage
+import os
+
 
 class StaticStorage(S3BotoStorage):
     location = settings.STATICFILES_LOCATION
 
 class MediaStorage(S3BotoStorage):
     location = settings.MEDIAFILES_LOCATION
+
+    def isdir(path):
+        return os.path.isdir(path)
